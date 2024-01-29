1053c44395c655e4b71569ea72c9edf03bd3bd43
Rinisha Ramprakash <69024782+rinisha03@users.noreply.github.com>
2024-01-29 18:10:59 -0500

    Add files via upload
    Create-React-App

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App() 
+  const name = 'Rinisha'
+
+  return (
+    <div className="App">
+      <h1>Hello {2+2}!</h1> 
+      {name ? (
+        <>
+          test
+        </>
+        ="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ npx create-react-app my-app.jsx
@@ -0,0 +1,36 @@
+npx create-react-app my-app
+npx create-react-app my-app --template typescript
+# Run this to use npm
+npx create-react-app my-app
+cd my-app
+# Run this to use npm
+npx create-react-app my-app
+# Or run this to use yarn
+yarn create react-app my-app
+my-app
+├── README.md
+├── node_modules
+├── package.json
+├── .gitignore
+├── public
+│   ├── favicon.ico
+│   ├── index.html
+│   ├── logo192.png
+│   ├── logo512.png
+│   ├── manifest.json
+│   └── robots.txt
+└── src
+    ├── App.css
+    ├── App.js
+    ├── App.test.js
+    ├── index.css
+    ├── index.js
+    ├── logo.svg
+    ├── serviceWorker.js
+    └── setupTests.js
+
+    cd my-app
+
+    npm start
+
+    npx create-remix
